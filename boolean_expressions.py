31a0f51caf0d77019bb021005217e7d87fe69f11
apaxonpython <archelaus05@gmail.com>
2024-09-06 11:00:32 -0500

    Update boolean_expressions.py

--- boolean_expressions.py
+++ boolean_expressions.py
@@ -1,1 +1,50 @@
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names:
+# Townsend Wheeler
+# Duc Pham
+# Julian Curry
+# Archelaus Paxon
+# Section: 518
+# Assignment: Lab Topic 4 Activity 3
+# Date:
 
+   
+############ Part A ############
+
+# create a, b, c (strings); ask for user input
+# check and convert a, b, c to their boolean variants
+# if a = "True" or "T" or "t" and NOT ("False" or "F" or "f")
+
+# example:
+a = input("Enter True or False for a: ")
+a = (a == "True" or a == "T" or a == "t") and not(a == "False" or a == "F" or a == "f")
+# print(a) # if you want to test it out
+
+############ Part B ############
+
+# print "a and b and c: " then check (a and b and c)
+# print "a or b or c: " then check (a or b or c)
+
+############ Part C ############
+
+# print "XOR: " then check (not a and b) or (not b and a)
+# print "Odd number: " then check the cases when...
+
+# all three are true: a and b and c
+# only a is true: a and (not b) and (not c)
+# only b is true: see a above, swap out letters
+# only c is true: see a above, swap out letters
+
+# evaluate all 4 cases above using <and> on ONE LINE OF CODE (probably inside the print statement; if one case fail, it should return False)
+
+############ Part D ############
+
+# notice how the parenthesis light up when you click on one of its pair
+
+# (not (a and not b) or (not c and b)) and (not b) or (not a and b and not c) or (a and not b)
+# (not ((b or not c) and (not a or not c))) or (not (c or not (b and c))) or (a and not c) and (not a or (a and b and c) or (a and ((b and not c) or (not b))))
+
+# ¯\_(ツ)_/¯ idk how to simplify these yet
