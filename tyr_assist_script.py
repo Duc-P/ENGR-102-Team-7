93355e755df74ef0d090d4fdbb1c5e6a9f2aeca3
apaxonpython <archelaus05@gmail.com>
2024-09-13 16:07:20 -0500

    Update tyr_assist_script.py

--- tyr_assist_script.py
+++ tyr_assist_script.py
@@ -84,29 +84,208 @@ def M_cho_point(cho,age):
         return 1
     elif (60 <= age <= 69) and (200 <= cho <= 239):
         return 1
+    elif (70 <= age <= 79) and (240 <= cho):
+        return 1
     # 2 points
-    
-    return 0
-def F_cho_point(cho):
-    return 0
+    elif (50 <= age <= 59) and (160 <= cho <= 199):
+        return 2
+    elif (60 <= age <= 69) and (240 <= cho <= 279):
+        return 2
+    #3 Points
+    elif (40 <= age <= 49) and (160 <= cho <= 199):
+        return 3
+    elif (50 <= age <= 59) and (200 <= cho <= 239):
+        return 3
+    elif (60 <= age <= 69) and (280 <= cho):
+        return 3
+    #4 Points
+    elif (20 <= age <= 39) and (160 <= cho <= 199):
+        return 4
+    elif (50 <= age <= 59) and (240 <= cho <= 279):
+        return 4
+    #5 points
+    elif (40 <= age <= 49) and (200 <= cho <= 239):
+        return 5
+    elif (50 <= age <= 59) and (280 <= cho):
+        return 5
+    #6 points
+    elif (40 <= age <= 49) and (240 <= cho <= 279):
+        return 6
+    #7 points
+    elif (20 <= age <= 39) and (160 <= cho <= 199):
+        return 7
+    #8 points
+    elif (40 <= age <= 49) and (280 <= cho):
+        return 8
+    #9 points
+    elif (20 <= age <= 39) and (240 <= cho <= 279):
+        return 9
+    #11 points
+    elif (20 <= age <= 39) and (280 <= cho):
+        return 11
+    else:
+        return 0
+
+def F_cho_point(cho, age):
+    #0 point
+    if cho < 160:
+        return 0
+    # 1 point
+    elif (60 <= age <= 69) and (160 <= cho <= 199):
+        return 1
+    elif (70 <= age <= 79) and (160 <= cho <= 199):
+        return 1
+    elif (70 <= age <= 79) and (200 <= cho <= 239):
+        return 1
+    # 2 points
+    elif (50 <= age <= 59) and (160 <= cho <= 199):
+        return 2
+    elif (60 <= age <= 69) and (200 <= cho <= 239):
+        return 2
+    elif (70 <= age <= 79) and (240 <= cho):
+        return 2
+    #3 Points
+    elif (60 <= age <= 69) and (240 <= cho <= 279):
+        return 3
+    elif (40 <= age <= 49) and (160 <= cho <= 199):
+        return 3
+    #4 Points
+    elif (20 <= age <= 39) and (160 <= cho <= 199):
+        return 4
+    elif (50 <= age <= 59) and (200 <= cho <= 239):
+        return 4
+    elif (60 <= age <= 69) and (280 <= cho):
+        return 4
+    #6 points
+    elif (40 <= age <= 49) and (200 <= cho <= 239):
+        return 6
+    #7 points
+    elif (50 <= age <= 59) and (280 <= cho):
+        return 7
+    #8 points
+    elif (20 <= age <= 39) and (200 <= cho <= 239):
+        return 8
+    elif (40 <= age <= 49) and (240 <= cho <= 279):
+        return 8
+    #10 points
+    elif (40 <= age <= 49) and (280 <= cho):
+        return 10
+    #11 points
+    elif (20 <= age <= 39) and (240 <= cho <= 279):
+        return 11
+    #13 points
+    elif (20 <= age <= 39) and (280 <= cho):
+        return 13
+    else:
+        return 0
 
 # non/smoker point helper
-def M_smo_point(smo):
-    return 0
-def F_smo_point(smo):
-    return 0
+def M_smo_point(smo, age):
+    if smo == "N":
+        return 0
+    elif smo == "Y" and (20 <= age <= 39):
+        return 8
+    elif smo == "Y" and (40 <= age <= 49):
+        return 5
+    elif smo == "Y" and (50 <= age <= 59):
+        return 3
+    elif smo == "Y" and (60 <= age <= 69):
+        return 1
+    elif smo == "Y" and (70 <= age <= 79):
+        return 1
+    else:
+        return 0
+
+def F_smo_point(smo, age):
+    if smo == "N":
+        return 0
+    elif smo == "Y" and (20 <= age <= 39):
+        return 9
+    elif smo == "Y" and (40 <= age <= 49):
+        return 7
+    elif smo == "Y" and (50 <= age <= 59):
+        return 4
+    elif smo == "Y" and (60 <= age <= 69):
+        return 2
+    elif smo == "Y" and (70 <= age <= 79):
+        return 1
+    else:
+        return 0
 
 # HDL point helper
 def M_hdl_point(hdl):
-    return 0
+    if (hdl >= 60):
+        return -1
+    elif (50 <= hdl <= 59):
+        return 0
+    elif (40 <= hdl <= 49):
+        return 1
+    elif (hdl < 40):
+        return 2
+
 def F_hdl_point(hdl):
-    return 0
+    if (hdl >= 60):
+        return -1
+    elif (50 <= hdl <= 59):
+        return 0
+    elif (40 <= hdl <= 49):
+        return 1
+    elif (hdl < 40):
+        return 2
 
 # Systolic BP and treatment
 def M_sbp_med_point(sbp,med):
-    return 0
+    if med == "Y":
+        if (sbp < 120):
+            return 0
+        elif (120 <= sbp <= 129):
+            return 1
+        elif (130 <= sbp <= 139):
+            return 2
+        elif (140 <= sbp <= 159):
+            return 2
+        elif (sbp >= 160):
+            return 3
+    elif med == "N":
+        if (sbp < 120):
+            return 0
+        elif (120 <= sbp <= 129):
+            return 0
+        elif (130 <= sbp <= 139):
+            return 1
+        elif (140 <= sbp <= 159):
+            return 1
+        elif (sbp >= 160):
+            return 2
+    else:
+        return 0
+
 def F_sbp_med_point(sbp,med):
-    return 0
+    if med == "Y":
+        if (sbp < 120):
+            return 0
+        elif (120 <= sbp <= 129):
+            return 3
+        elif (130 <= sbp <= 139):
+            return 4
+        elif (140 <= sbp <= 159):
+            return 5
+        elif (sbp >= 160):
+            return 6
+    elif med == "N":
+        if (sbp < 120):
+            return 0
+        elif (120 <= sbp <= 129):
+            return 1
+        elif (130 <= sbp <= 139):
+            return 2
+        elif (140 <= sbp <= 159):
+            return 3
+        elif (sbp >= 160):
+            return 4
+    else:
+        return 0
+
 
 # point calculations
 def point_calc(pc_sex, pc_age, pc_cho, pc_smo, pc_hdl, pc_sbp, pc_med):
@@ -115,20 +294,97 @@ def point_calc(pc_sex, pc_age, pc_cho, pc_smo, pc_hdl, pc_sbp, pc_med):
     if pc_sex == "M":
         points += M_age_point(pc_age)
         points += M_cho_point(pc_cho,pc_age)
-        points += M_smo_point(pc_smo)
+        points += M_smo_point(pc_smo,pc_age)
         points += M_hdl_point(pc_hdl)
         points += M_sbp_med_point(pc_sbp,pc_med)
+        return points
         
     elif pc_sex == "F":
         points += F_age_point(pc_age)
         points += F_cho_point(pc_cho,pc_age)
-        points += F_smo_point(pc_smo)
+        points += F_smo_point(pc_smo,pc_age)
         points += F_hdl_point(pc_hdl)
         points += F_sbp_med_point(pc_sbp,pc_med)
+        return(points)
 
 # output calculations
-def out_calc (oc_point): # input point and return expected output (%)
-    return 0
+def out_calc (oc_point, sex): # input point and return expected output (%)
+    if sex == "M":
+        if oc_point < 0:
+            return "<1"
+        elif oc_point == 0:
+            return 1
+        elif oc_point == 1:
+            return 1
+        elif oc_point == 2:
+            return 1
+        elif oc_point == 3:
+            return 1
+        elif oc_point == 4:
+            return 1
+        elif oc_point == 5:
+            return 2
+        elif oc_point == 6:
+            return 2
+        elif oc_point == 7:
+            return 3
+        elif oc_point == 8:
+            return 4
+        elif oc_point == 9:
+            return 5
+        elif oc_point == 10:
+            return 6
+        elif oc_point == 11:
+            return 8
+        elif oc_point == 12:
+            return 10
+        elif oc_point == 13:
+            return 12
+        elif oc_point == 14:
+            return 16
+        elif oc_point == 15:
+            return 20
+        elif oc_point == 16:
+            return 25
+        elif oc_point >= 17:
+            return ">30"
+    else:
+        if oc_point < 9:
+            return "<1"
+        elif oc_point == 9:
+            return 1
+        elif oc_point == 10:
+            return 1
+        elif oc_point == 11:
+            return 1
+        elif oc_point == 12:
+            return 1
+        elif oc_point == 13:
+            return 2
+        elif oc_point == 14:
+            return 2
+        elif oc_point == 15:
+            return 3
+        elif oc_point == 16:
+            return 4
+        elif oc_point == 17:
+            return 5
+        elif oc_point == 18:
+            return 6
+        elif oc_point == 19:
+            return 8
+        elif oc_point == 20:
+            return 11
+        elif oc_point == 21:
+            return 14
+        elif oc_point == 22:
+            return 17
+        elif oc_point == 23:
+            return 22
+        elif oc_point == 24:
+            return 27
+        elif oc_point >= 25:
+            return ">30"
 
 # loop generate random strings
 for i in range (0,num_case):
@@ -146,18 +402,17 @@ for i in range (0,num_case):
     elif random.random() >= 0.5:
         smo = "Y"
     
-    hdl = random.randrange(0,120) # will tweak later once range is reasonable
+    hdl = random.randrange(0,70) # will tweak later once range is reasonable
     
-    sbp = random.randrange(0,320) # will tweak later once range is reasonable
+    sbp = random.randrange(0,170) # will tweak later once range is reasonable
     
     if random.random() < 0.5:
         med = "N"
     elif random.random() >= 0.5:
         med = "Y"
         
-    point_calc(sex,age,cho,smo,hdl,sbp,med)
-        
-    #case_patient = f"sex:{sex} age:{age} cho:{cho} smo:{smo} hdl:{hdl} sbp:{sbp} med:{med} out:{out}"
-    #print(case_patient)
+    point = point_calc(sex,age,cho,smo,hdl,sbp,med)
+    out = out_calc(point, sex)
     
-    
+    case_patient = f"sex:{sex} age:{age} cho:{cho} smo:{smo} hdl:{hdl} sbp:{sbp} med:{med} out:{out}"
+    print(case_patient)
