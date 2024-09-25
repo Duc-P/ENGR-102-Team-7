925de838e9b78ed58c5bea80e3736a838c0943ab
townsend-wheeler <tmwheeler28@gmail.com>
2024-09-25 15:58:12 -0500

    Update go_moves.py

--- go_moves.py
+++ go_moves.py
@@ -20,7 +20,7 @@ for i in board:
     for j in i:
         print(j,"",end="")
     print()
-"""
+
 
 board = [[".",".",".",".",".",".",".",".","."],
          [".",".",".",".",".",".",".",".","."],
@@ -43,4 +43,53 @@ while user_row != "stop" or user_col != "stop":
         print()
     user_row = input("Enter Row: ")
     user_col = input("Enter Column: ")
-    board[int(user_row) - 1][int(user_col) - 1] = chr(9675)
+    board[int(user_row) - 1][int(user_col) - 1] = chr(9675)"""
+
+    board = [[".",".",".",".",".",".",".",".","."],
+         [".",".",".",".",".",".",".",".","."],
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."], 
+         [".",".",".",".",".",".",".",".","."]]
+
+turn = False # flip flop b/w True and False
+user_row = 1 # first input 
+user_col = 1 # second input
+
+while user_row != "stop" or user_col != "stop": 
+    if turn == False:
+        user_column = int(input('Player 1 Choose your desired column:' ))
+        user_row = int(input('Player 1 Choose your desired row:'))
+        if user_column == 'stop' or user_row == 'stop':
+            break
+        board[user_row-1][user_column-1]
+        while board[user_row-1][user_column-1] == chr(9675) or board[user_row-1][user_column-1] == chr(9679):
+            print('This Space has already been played, choose a different place!')
+            user_column = int(input('Player 1 Choose your desired column:' ))
+            user_row = int(input('Player 1 Choose your desired row:'))
+        board[user_row-1][user_column-1] = chr(9679)
+        turn = True
+        for i in board:
+            for j in i:
+                print(j,"",end="")
+            print()
+    else:
+        user_column = int(input('Player 2 Choose your desired column:' ))
+        user_row = int(input('Player 2 choose your desired row:'))
+        if user_column == 'stop' or user_row == 'stop':
+            break
+        board[user_row-1][user_column-1]
+        while board[user_row-1][user_column-1] == chr(9675) or board[user_row-1][user_column-1] == chr(9679):
+            print('This Space has already been played, choose a different place!')
+            user_column = int(input('Player 1 Choose your desired column:' ))
+            user_row = int(input('Player 1 Choose your desired row:'))
+        board[user_row-1][user_column-1] =chr(9675)
+        turn = False
+        for i in board:
+            for j in i:
+                print(j,"",end="")
+            print()
+
