d974e5db532e5323e0d94786d50dba5106dd1515
apaxonpython <archelaus05@gmail.com>
2024-10-13 18:50:50 -0500

    Update ascii_clock.py

--- ascii_clock.py
+++ ascii_clock.py
@@ -136,19 +136,19 @@ while ":" not in time: #Check if time has a colon
 time_list = time.split(":")
 #Check if the hour is between 0 to 24
 while int(time_list[0]) < 0 or int(time_list[0]) > 24:
-    time = input("Please enter a valid time1! Enter the time: ")
+    time = input("Please enter a valid time! Enter the time: ")
     time_list = time.split(":")
 #check if the minutes is between 0 - 60
 while int(time_list[1]) < 0 or int(time_list[1]) > 60:
-    time = input("Please enter a valid time2! Enter the time: ")
+    time = input("Please enter a valid time! Enter the time: ")
     time_list = time.split(":")
 #Check if first half of time has 1 or 2 digits
 while len(time_list[0]) not in [1,2]:
-    time = input("Please enter a valid time3! Enter the time: ")
+    time = input("Please enter a valid time! Enter the time: ")
     time_list = time.split(":")
 #Check if second half of time has 2 digits
 while len(time_list[1]) != 2:
-    time = input("Please enter a valid time4! Enter the time: ")
+    time = input("Please enter a valid time! Enter the time: ")
     time_list = time.split(":")
 #Store the hour and minutes separately
 hour = time_list[0]
