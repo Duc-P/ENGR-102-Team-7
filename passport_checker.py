eeb59e65c6123cac828a222156094ce783cb3a68
townsend-wheeler <tmwheeler28@gmail.com>
2024-10-30 16:02:33 -0500

    Update passport_checker.py

--- passport_checker.py
+++ passport_checker.py
@@ -1,9 +1,7 @@
 #take in user input
 user_query = input("Enter the name of the file: ")
 ORIGINAL_PASS = open(user_query, "r+")
-
-with ORIGINAL_PASS as passer:
-    for passer_line in passer:
-        element = passer_line.split()
-        if element != []:
-            print(element)
+pass_string = ORIGINAL_PASS.read()
+ORIGINAL_PASS.close()
+pass_list = pass_string.split('\n\n')
+print(pass_list)
