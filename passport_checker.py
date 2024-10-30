07067db6a65578e52618cdd6e418e8d1c5a15a0f
Duc Pham <156979391+Duc-P@users.noreply.github.com>
2024-10-30 15:11:40 -0500

    Update passport_checker.py

--- passport_checker.py
+++ passport_checker.py
@@ -1,1 +1,9 @@
-#take in user inputuser_query = input("Enter the name of the file: ")ORIGINAL_PASS = open(user_query, "r+")with ORIGINAL_PASS as passer:    for passer_line in passer:        element = passer_line.split()        print(element)
+#take in user input
+user_query = input("Enter the name of the file: ")
+ORIGINAL_PASS = open(user_query, "r+")
+
+with ORIGINAL_PASS as passer:
+    for passer_line in passer:
+        element = passer_line.split()
+        if element != []:
+            print(element)
