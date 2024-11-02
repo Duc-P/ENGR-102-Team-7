d1b27377d46f959db55b45369b619b4d72cf1e34
townsend-wheeler <tmwheeler28@gmail.com>
2024-11-02 17:06:17 -0500

    Update passport_checker2_WIP.py

--- passport_checker2_WIP.py
+++ passport_checker2_WIP.py
@@ -10,12 +10,19 @@
 # Section: 518
 # Assignment: Lab 11.10: Passport Checker Part B
 # Date: 2-11-2024
-
+ecl_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def check_cid(passport):
     if 'cid' in passport:
         cid_value = int(passport[passport.index('cid')+4:passport.index('cid')+7])
+        
         #print(cid_value)
         if 99 < cid_value < 1000:
+            try:
+                int(passport[(passport.index('cid')+8):(passport.index('cid')+8)])
+            except:
+                return True
+            else:
+                return False
             return True
     return False
 
@@ -23,10 +30,14 @@ def check_pid(passport):
     pid_bool = False
     if 'pid' in passport:
         pid_value = str(passport[passport.index('pid')+4:passport.index('pid')+13]) # generate string of interest
-        if pid_value[-1] == " " or pid_value[-1] == "\n" or pid_value[-1] == "\t":   
-            return False # when detect a space, newline, or tab
-        else:
-            pid_bool = True
+        try:
+            int(passport[passport.index('pid')+14:passport.index('pid')+14])
+        except:
+            if pid_value[-1] == " " or pid_value[-1] == "\n" or pid_value[-1] == "\t":   
+                return False # when detect a space, newline, or tab
+            else:
+                pid_bool = True
+        else: pid_bool = False
     return pid_bool
 
 def check_ecl(passport):
@@ -34,8 +45,15 @@ def check_ecl(passport):
     
     if 'ecl' in passport:
         ecl_value = str(passport[passport.index('ecl')+4:passport.index('ecl')+7])
-        if ecl_value in valid_ecl:
-            return True
+        try:
+            int(passport[passport.index('ecl')+8:passport.index('ecl')+8])
+        except:
+            if str(passport[passport.index('ecl')+8:passport.index('ecl')+8]) in ecl_char:
+                return False
+            if ecl_value in valid_ecl:
+                return True
+        else:
+            return False
     return False
 
 def check_hcl(passport):
@@ -47,6 +65,8 @@ def check_hcl(passport):
         #print("hair color code: ",hcl_value) # the string of interest for further analysis
         if hcl_value[0] == "#":
             #print("pass the # test")
+            if str(passport[passport.index('hcl')+12:passport.index('hcl')+12]) in valid_char:
+                return False
             for i in range(1,7):
                 if not(hcl_value[i] in valid_char):
                     #print("the value that failed: ",hcl_value[i])
@@ -58,18 +78,38 @@ def check_hcl(passport):
 def check_hgt(passport):
     if 'hgt' in passport:
         if 'cm' in passport and (150 <= int(passport[passport.index('cm')-3:passport.index('cm')]) <= 193):
-            return True # parsing the 3 digits before the "cm" in hgt and converting it to an int for comparison
+            try:
+                int(passport[passport.index('cm')-4])
+            except:
+                return True # parsing the 3 digits before the "cm" in hgt and converting it to an int for comparison
         elif 'in' in passport and (59 <= int(passport[passport.index('in')-2:passport.index('in')]) <= 76):
-            return True # parsing the 2 digits before the "in" in hgt and converting it to an int for comparison
+            try:
+                int(passport[passport.index('in')-3])
+            except:
+                return True # parsing the 2 digits before the "in" in hgt and converting it to an int for comparison
     return False
 
 def check_eyr(passport):
     if 'eyr' in passport and (2024 <= int(passport[passport.index('eyr')+4:passport.index('eyr')+8]) <= 2034):
+        try:
+            int(passport[(passport.index('eyr')+9):(passport.index('eyr')+9)])
+        except:
+            return True
+        else:
+            return False
         return True # parsing the 4 digits after the ":" in eyr and converting it to an int for comparison
     return False
 
 def check_byr(passport):# take in one passport 
     if 'byr' in passport and (1920 <= int(passport[(passport.index('byr')+4):(passport.index('byr')+8)]) <= 2008):
+        try:
+            int(passport[(passport.index('byr')+9):(passport.index('byr')+9)])
+        except:
+            return True
+        else:
+            return False
+        
+        
         return True # parsing the 4 digits after the ":" in byr and converting it to an int for comparison
     return False
 
@@ -80,7 +120,7 @@ pass_string = ORIGINAL_PASS.read()
 ORIGINAL_PASS.close()
 pass_list = pass_string.split('\n\n')
 valid_passports = 0
-
+#print(pass_list)
 for i in pass_list:
     if check_byr(i):
         if check_eyr(i):
